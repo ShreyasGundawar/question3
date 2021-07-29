2ca9aeaf21c85c701e59c69a22a60fe3fa0702c8
root <shreyasrc27@gmail.com>
2021-07-29 13:05:32 +0530

    first commit

--- program.c
+++ program.c
@@ -3,7 +3,7 @@ int main()
 {    
     int sg1,sg2,sum;
     printf("Enter two integers: ");
-    scanf("%d %d", &sg1,&sg22);
+    scanf("%d %d", &sg1,&sg2);
     sum = sg1+sg2;          
     printf("%d + %d = %d",sg1,sg2,sum);
     return 0;
